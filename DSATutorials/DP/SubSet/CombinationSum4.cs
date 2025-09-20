5b3921ad415fbdd3023fae5f6b37e5794b080a7d
Rohit Shukla <rohitshukla@microsoft.com>
2025-09-20 23:18:28 +0530

    [New code - 20-09-2025]

---
+++ DSATutorials/DP/SubSet/CombinationSum4.cs
@@ -0,0 +1,146 @@
+﻿
+//class Solution
+//{
+//    public int CombinationSum4(int[] nums, int target)
+//    {
+//        // Recursion:
+//        //return Solve(nums, 0, target);
+
+//        // Memoization with 2D DP
+//        //int[,] dp = new int[nums.Length + 1, target + 1];
+
+//        //for (int i = 0; i < dp.GetLength(0); i++)
+//        //{
+//        //    for (int j = 0; j < dp.GetLength(1); j++)
+//        //    {
+//        //        dp[i, j] = -1;
+//        //    }
+//        //}
+
+
+//        // Memiozation with 1D DP
+//        int[] dp = new int[target + 1];
+//        Array.Fill(dp, -1);
+
+//        //return Solve(nums, target, dp);
+
+//        return Solve(nums, target);
+//    }
+
+//    // Time :  O(2^n) , space : O(n)
+//    private int Solve(int[] nums, int idx, int target)
+//    {
+//        // base case
+//        // 1. Target achieved
+//        if (target == 0)
+//        {
+//            return 1;
+//        }
+
+//        // 2. If the target or idx goes over or under bound
+//        if (idx >= nums.Length || target < 0)
+//        {
+//            return 0;
+//        }
+
+//        // We will jump back to first index to start over (special case)
+//        int take = Solve(nums, 0, target - nums[idx]);
+
+//        int notTake = Solve(nums, idx + 1, target);
+
+//        return take + notTake;
+//    }
+
+//    // Memoized with 2D Dp
+//    private int Solve(int[] nums, int idx, int target, int[,] dp)
+//    {
+//        // base case
+//        // 1. Target achieved
+//        if (target == 0)
+//        {
+//            return 1;
+//        }
+
+//        // 2. If the target or idx goes over or under bound
+//        if (idx >= nums.Length || target < 0)
+//        {
+//            return 0;
+//        }
+
+//        if (dp[idx, target] != -1)
+//        {
+//            return dp[idx, target];
+//        }
+
+//        // We will jump back to first index to start over (special case)
+//        int take = Solve(nums, 0, target - nums[idx], dp);
+
+//        int notTake = Solve(nums, idx + 1, target, dp);
+
+//        return dp[idx, target] = take + notTake;
+//    }
+
+//    // Memoized with 1D Dp
+//    // Create a recursion tree and it will be super simple for you to understand
+//    private int Solve(int[] nums, int target, int[] dp)
+//    {
+//        // base case
+//        if (target == 0)
+//        {
+//            return 1;
+//        }
+
+//        if (target < 0)
+//        {
+//            return 0;
+//        }
+
+//        if (dp[target] != -1)
+//        {
+//            return dp[target];
+//        }
+
+//        int ways = 0;
+//        foreach (int num in nums)
+//        {
+//            ways += Solve(nums, target - nums[num], dp);
+//        }
+
+//        return ways;
+//    }
+
+//    // Tabulation , time  : O(n*m) , space :O(target)
+//    private int Solve(int[] nums, int target)
+//    {
+//        int[] dp = new int[target + 1];
+
+//        dp[0] = 1;
+
+//        for (int tar = 1; tar <= target; tar++)
+//        {
+//            int ways = 0;
+//            foreach (int idx in nums)
+//            {
+//                if (tar - idx >= 0)
+//                {
+//                    ways += dp[tar - idx];
+//                }
+//            }
+//            dp[tar] = ways;
+//        }
+
+//        return dp[target];
+//    }
+//}
+
+//class Program
+//{
+//    public static void Main()
+//    {
+//        int[] nums = { 1, 2, 3 };
+//        int target = 4;
+
+//        Solution s = new Solution();
+//        Console.WriteLine(s.CombinationSum4(nums, target));
+//    }
+//}
---
+++ DSATutorials/Recursion/CombinationSum2.cs
@@ -0,0 +1,78 @@
+﻿
+//using System.Collections.Generic;
+
+//class Solution
+//{
+//    public IList<IList<int>> CombinationSum2(int[] candidates, int target)
+//    {
+//        IList<IList<int>> result = new List<IList<int>>();
+
+//        IList<int> current = new List<int>();
+
+//        Array.Sort(candidates);
+
+//        Solve(candidates, target, 0, result, current);
+
+//        return result;
+//    }
+
+//    // Time : O(2^n * n) + O(nlogn) , space :O(n)
+//    private void Solve(int[] candidates, int target, int idx, IList<IList<int>> result, IList<int> current)
+//    {
+//        // base case
+//        if (target == 0)
+//        {
+//            result.Add(new List<int>(current));
+//            return;
+//        }
+
+//        if (idx >= candidates.Length)
+//        {
+//            return;
+//        }
+
+//        // Here pick condition is bit different : Basically this is another way of working which kind of does the same thing
+//        for (int i = idx; i < candidates.Length; i++)
+//        {
+//            // Since array is already sorted we are good and no point to move further
+//            if (candidates[i] > target) break;
+
+//            // Avoid duplicates by making sure that at same level of recursion we skip the duplicate but if level changes we should take 
+//            if (i > idx && candidates[i] == candidates[i - 1])
+//            {
+//                continue;
+//            }
+
+//            current.Add(candidates[i]);
+//            Solve(candidates, target - candidates[i], i + 1, result, current);
+//            current.RemoveAt(current.Count - 1);
+
+//            // bascially the for loop does the work of not take already
+//        }
+//    }
+
+
+
+//}
+//class Program
+//{
+//    public static void Main()
+//    {
+//        int[] candidates = { 10, 1, 2, 7, 6, 1, 5 };
+//        int target = 8;
+
+//        Solution s = new Solution();
+
+//        var result = s.CombinationSum2(candidates, target);
+
+//        for (int i = 0; i < result.Count; i++)
+//        {
+//            foreach (int num in result[i])
+//            {
+//                Console.Write($"{num}" + " ");
+//            }
+//            Console.WriteLine();
+//        }
+//    }
+
+//}
